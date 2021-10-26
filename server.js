17ffb3b141cd65bd8aee0f72ffa47fe213600fe3
Dmitriy <dmitriyolv@gmail.com>
2021-10-26 23:53:48 +0300

    heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./server.json');
+const middlewares = jsonServer.defaults({
+  static: './dist'
+});
+const PORT = process.env.PORT || 'https://foodmyproject.herokuapp.com/app';
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
